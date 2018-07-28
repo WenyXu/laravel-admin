69d45ece25507ecc2618970a83c9aa8d06d4ae22
wenyxu <wenymedia@gmail.com>
2018-07-28 22:03:43 +0900

    changed img

--- src/Auth/Database/HasPermissions.php
+++ src/Auth/Database/HasPermissions.php
@@ -21,7 +21,7 @@ trait HasPermissions
             return Storage::disk(config('admin.upload.disk'))->url($avatar);
         }
 
-        return admin_asset('/vendor/laravel-admin/AdminLTE/dist/img/user2-160x160.jpg');
+        return admin_asset('/vendor/laravel-admin/AdminLTE/dist/img/200_200.jpg');
     }
 
     /**
