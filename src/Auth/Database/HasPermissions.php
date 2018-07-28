ba63c6149cb9caddf96cc970d20597eb4a99e667
wenyxu <wenymedia@gmail.com>
2018-07-28 22:22:41 +0900

    add

--- src/Auth/Database/HasPermissions.php
+++ src/Auth/Database/HasPermissions.php
@@ -20,6 +20,7 @@ trait HasPermissions
         if ($avatar) {
             return Storage::disk(config('admin.upload.disk'))->url($avatar);
         }
+        
         return admin_asset('/vendor/laravel-admin/AdminLTE/dist/img/200_200.jpg');
     }
 
